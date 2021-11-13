d0b1777c90e11f0a5858975f5069c60b67274fdd
Guilherme Silva <guilherme.dna06@gmail.com>
2021-11-13 11:17:39 -0300

    Mudança de localidade para o Brasil

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,8 @@ module GerenciamentoES
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    config.i18n.default_locale = :"pt-BR"
+    I18n.enforce_available_locales = false
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
