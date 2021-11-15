41ceec8c522442a3d6f04b5cc4d74ab4c394dd07
WevertonCintra <wevertoncintra@outlook.com>
2021-11-15 10:15:57 -0300

    Adicionando testes de models

--- test/models/encomenda_test.rb
+++ test/models/encomenda_test.rb
@@ -1,7 +1,37 @@
 require "test_helper"
 
 class EncomendaTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar encomenda valida" do
+    funcionario = Funcionario.new(nome: "zeze", email: "zeze@email.com", senha: "S&nha123", senha_confirma: "S&nha123")
+    assert funcionario.save
+
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert morador.save
+
+    encomenda = Encomenda.new(remetente: "amazon", data: "01-10-2021", funcionario_id: funcionario.id, morador_id: morador.id)
+    assert encomenda.save
+  end
+
+  test "criar encomenda invalida" do
+    funcionario = Funcionario.new(nome: "zeze", email: "zeze@email.com", senha: "S&nha123", senha_confirma: "S&nha123")
+    assert funcionario.save
+
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert morador.save
+
+    encomenda = Encomenda.new(remetente: "", data: "", funcionario_id: funcionario.id, morador_id: morador.id)
+    assert encomenda.save
+  end
+
+  test "excluir encomenda" do
+    funcionario = Funcionario.new(nome: "zeze", email: "zeze@email.com", senha: "S&nha123", senha_confirma: "S&nha123")
+    assert funcionario.save
+
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert morador.save
+
+    encomenda = Encomenda.new(remetente: "amazon", data: "01-10-2021", funcionario_id: funcionario.id, morador_id: morador.id)
+    assert encomenda.save
+
+    assert encomenda.destroy
 end
--- test/models/morador_test.rb
+++ test/models/morador_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class MoradorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "criar morador valido" do
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert morador.save
+  end
+
+  test "criar morador invalido" do
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert_not morador.save
+  end
+
+  test "Atualizar morador" do
+    morador = Morador.new(nome: "zeze", cpf: "111.111.111-00", contato: "(81)99999-9999", bloco: "A", casanum: "01")
+    assert morador.save
+    assert morador.update(name: "zeze junior")
+  end
 end
