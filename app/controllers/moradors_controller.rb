fcd5c2042dcaa9c619a854f20b932e7e5b160826
Guilherme Silva <guilherme.dna06@gmail.com>
2021-11-29 08:27:51 -0300

    Ajustes nas páginas para diferenciação do acesso as páginas

--- app/controllers/moradors_controller.rb
+++ app/controllers/moradors_controller.rb
@@ -1,6 +1,6 @@
 class MoradorsController < ApplicationController
   before_action :set_morador, only: %i[ show edit update destroy ]
-
+  before_action :logged_in_user
   # GET /moradors or /moradors.json
   def index
     @moradors = Morador.all
