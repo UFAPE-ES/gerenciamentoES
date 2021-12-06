d9f64a9b7a4b810429f1b6b472c0fda62cc25ba3
unknown <antoniodurvalc@gmail.com>
2021-12-06 16:30:51 -0300

    Adicionando tests de model do admin

--- test/models/admin_test.rb
+++ test/models/admin_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 class AdminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'admin sem nome' do
+    admin = Admin.new(nome: '', email: 'admin002@admin.com', password:'123456', password_confirmation:'123456')
+    assert_not admin.save
+  end
+
+  test 'admin valido' do
+    admin = Admin.new(nome: 'Tonhao', email: 'admin002@admin.com', password:'123456', password_confirmation:'123456')
+    assert admin.save
+  end
+
+  test 'admin sem email ' do
+    admin = Admin.new(nome: 'Tonhao', email: '', password:'123456', password_confirmation:'123456')
+    assert_not admin.save
+  end
+
 end
