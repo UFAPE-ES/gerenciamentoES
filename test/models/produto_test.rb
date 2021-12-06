66c52792ce52347a7f20ba9c6a08f235507d25dd
unknown <antoniodurvalc@gmail.com>
2021-12-06 18:02:38 -0300

    Corrigindo test models de encomenda, endereco e produto

--- test/models/produto_test.rb
+++ test/models/produto_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class ProdutoTest < ActiveSupport::TestCase
   test "criar produto valido" do
-    user = User.new(nome:"pepe junior moreno", email:"pepe@gmail.com", tipo:"Funcionario", password:"admin1", password_confirmation:"admin1")
-    assert user.save
+    funcionario = Funcionario.new(nome:"pepe junior moreno", email:"pepe@gmail.com", password:"admin1", password_confirmation:"admin1")
+    assert funcionario.save
 
     morador = Morador.new(nome:"junior", cpf:"543.676.570-00", contato:"999999999", bloco:"A", casanum:"01")
     assert morador.save
@@ -11,7 +11,7 @@ class ProdutoTest < ActiveSupport::TestCase
     endereco = Endereco.new(rua:"rua central", complemento:"centro", cep:"55380-000", morador_id: morador.id)
     assert endereco.save
 
-    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", user_id:user.id, morador_id:morador.id)
+    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", funcionario_id:funcionario.id, morador_id:morador.id)
     assert encomenda.save
 
     produto = Produto.new(nome:"monitor", encomenda_id:encomenda.id)
@@ -19,8 +19,8 @@ class ProdutoTest < ActiveSupport::TestCase
   end
 
   test "criar produto com dado invalido" do
-    user = User.new(nome:"junior", email:"junior@gmail.com", tipo:"Funcionario", password:"admin1", password_confirmation:"admin1")
-    assert user.save
+    funcionario = Funcionario.new(nome:"pepe junior moreno", email:"pepe@gmail.com", password:"admin1", password_confirmation:"admin1")
+    assert funcionario.save
 
     morador = Morador.new(nome:"pepe junior", cpf:"995.629.430-62", contato:"999999999", bloco:"A", casanum:"01")
     assert morador.save
@@ -28,7 +28,7 @@ class ProdutoTest < ActiveSupport::TestCase
     endereco = Endereco.new(rua:"rua central", complemento:"centro", cep:"55380-000", morador_id: morador.id)
     assert endereco.save
 
-    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", user_id:user.id, morador_id:morador.id)
+    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", funcionario_id:funcionario.id, morador_id:morador.id)
     assert encomenda.save
 
     produto = Produto.new(nome:"", encomenda_id:encomenda.id)
@@ -36,8 +36,8 @@ class ProdutoTest < ActiveSupport::TestCase
   end
 
   test "atualizar produto" do
-    user = User.new(nome:"zeze filho", email:"zeze@gmail.com", tipo:"Funcionario", password:"admin1", password_confirmation:"admin1")
-    assert user.save
+    funcionario = Funcionario.new(nome:"pepe junior moreno", email:"pepe@gmail.com", password:"admin1", password_confirmation:"admin1")
+    assert funcionario.save
 
     morador = Morador.new(nome:"pepe moreno", cpf:"485.860.560-49", contato:"999999999", bloco:"A", casanum:"01")
     assert morador.save
@@ -45,7 +45,7 @@ class ProdutoTest < ActiveSupport::TestCase
     endereco = Endereco.new(rua:"rua central", complemento:"centro", cep:"55380-000", morador_id: morador.id)
     assert endereco.save
 
-    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", user_id:user.id, morador_id:morador.id)
+    encomenda = Encomenda.new(remetente:"Amazon", data: "30-12-2021", funcionario_id:funcionario.id, morador_id:morador.id)
     assert encomenda.save
 
     produto = Produto.new(nome:"Amazon", encomenda_id:encomenda.id)
