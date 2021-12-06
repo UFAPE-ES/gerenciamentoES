c98b958af354a9752da31c390894f4536edbc4dc
Guilherme Silva <guilherme.dna06@gmail.com>
2021-12-06 17:07:52 -0300

    Mudanças no seed para adapatção do funcionario

--- db/schema.rb
+++ db/schema.rb
@@ -28,12 +28,12 @@ ActiveRecord::Schema.define(version: 2021_12_04_175752) do
   create_table "encomendas", force: :cascade do |t|
     t.string "remetente"
     t.date "data"
-    t.integer "user_id", null: false
+    t.integer "funcionario_id", null: false
     t.integer "morador_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["funcionario_id"], name: "index_encomendas_on_funcionario_id"
     t.index ["morador_id"], name: "index_encomendas_on_morador_id"
-    t.index ["user_id"], name: "index_encomendas_on_user_id"
   end
 
   create_table "enderecos", force: :cascade do |t|
@@ -44,6 +44,19 @@ ActiveRecord::Schema.define(version: 2021_12_04_175752) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["morador_id"], name: "index_enderecos_on_morador_id"
+  end
+
+  create_table "funcionarios", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "nome"
+    t.index ["email"], name: "index_funcionarios_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_funcionarios_on_reset_password_token", unique: true
   end
 
   create_table "moradors", force: :cascade do |t|
@@ -64,22 +77,8 @@ ActiveRecord::Schema.define(version: 2021_12_04_175752) do
     t.index ["encomenda_id"], name: "index_produtos_on_encomenda_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "nome"
-    t.string "tipo"
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
+  add_foreign_key "encomendas", "funcionarios"
   add_foreign_key "encomendas", "moradors"
-  add_foreign_key "encomendas", "users"
   add_foreign_key "enderecos", "moradors"
   add_foreign_key "produtos", "encomendas"
 end
