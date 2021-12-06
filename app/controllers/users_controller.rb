bc723e9b66a29b1006f186ba774971331eb22f2d
Guilherme Silva <guilherme.dna06@gmail.com>
2021-12-06 11:43:19 -0300

    Adicionado deletar funcionario

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user
+  before_action :logged_in_admin
   def index
     @users = User.all
   end
@@ -26,12 +26,22 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_index_url, notice: "Usuario was successfully destroyed." }
+      format.html { redirect_to users_index_path	, notice: "Usuario apagado com sucesso." }
       format.json { head :no_content }
     end
   end
+
+  def deletar
+    @user = User.where("id = ?", params[:id]).first
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to root_path(:notice =>"Usuário deletado com sucesso")}
+    end
+  end
+
   private
     def user_params
       params.require(:user).permit(:nome, :password, :password_confirmation, :tipo)
     end
-end
+  end
+
