d45340fd436f2c373805194bf79cc577579cb339
Guilherme Silva <guilherme.dna06@gmail.com>
2021-12-06 11:15:59 -0300

    Alteração nas rotas

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,19 @@
 Rails.application.routes.draw do
+  namespace :site do
+    get 'welcome/index'
+  end
+  devise_for :admins
+  #cotrollers:{
+  # sessions: 'admins/sessions'
+  #}
+  devise_for :users
   get 'users/index'
   get 'users/show'
-  devise_for :users
+  get 'users/deletar'
+  delete "users/:id", to: "users#deletar"
   resources :encomendas
   resources :produtos
   resources :moradors
   resources :funcionarios
-  root to: "welcome#index"
+  root to: "site/welcome#index"
 end
