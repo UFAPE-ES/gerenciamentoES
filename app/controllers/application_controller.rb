5756f5e1692235dcd2a444b688b9e9faed60da0e
Guilherme Silva <guilherme.dna06@gmail.com>
2021-12-06 17:06:35 -0300

    Mudanças na autenticação e recebimento de parametro do funcionario e admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,18 +2,18 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   protected
   def configure_permitted_parameters
-    attributes = [:nome,:tipo]
+    attributes = [:nome]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
 
   # Returns true if the user is logged in, false otherwise.
   def logged_in?
-    !current_user.nil?
+    !current_funcionario.nil?
   end
 
   # Confirms a logged-in user.
-  def logged_in_user
+  def logged_in_funcionario
     unless logged_in?
       flash[:danger] = "Por favor, logue como funcionario para acessar"
       redirect_to new_user_session_path
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def logged?
-    !current_admin.nil? || !current_admin.nil?
+    !current_admin.nil?
   end
 
   # Confirms a logged-in admin.
@@ -33,13 +33,13 @@ class ApplicationController < ActionController::Base
   end
 
   def logado
-    !current_admin.nil? || !current_user.nil?
+    !current_admin.nil? || !current_funcionario.nil?
   end
 
   def logged_in
     unless logado
       flash[:danger] = "Por favor, logue como funcionario para acessar"
-      redirect_to new_user_session_path
+      redirect_to new_funcionario_session_path
     end
   end
 end
